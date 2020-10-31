e8dd7345f74cacc09efb8169e69685c893ffe275
gugupilot <73624237+gugupilot@users.noreply.github.com>
2020-10-31 12:38:53 -0400

    Update panda_mdpstype0.sh

--- _backup/panda_mdpstype0.sh
+++ _backup/panda_mdpstype0.sh
@@ -7,19 +7,19 @@ export PYTHONPATH=/data/openpilot
 
 cd /data/openpilot/_backup/mdpsH && rm *.*
 
-cd /data/openpilot/_backup/mdpsH && wget https://raw.githubusercontent.com/xps-genesis/openpilot/Xps_Community_Release/panda/board/drivers/can.h;
+cd /data/openpilot/_backup/mdpsH && wget https://raw.githubusercontent.com/gugupilot/openpilot/rel2/panda/board/drivers/can.h;
 cd /data/openpilot/_backup/mdpsH && cp can.h /data/openpilot/panda/board/drivers;
 
-cd /data/openpilot/_backup/mdpsH && wget https://raw.githubusercontent.com/xps-genesis/openpilot/Xps_Community_Release/panda/board/main.c;
+cd /data/openpilot/_backup/mdpsH && wget https://raw.githubusercontent.com/gugupilot/openpilot/rel2/panda/board/main.c;
 cd /data/openpilot/_backup/mdpsH && cp main.c /data/openpilot/panda/board;
 
-cd /data/openpilot/_backup/mdpsH && wget https://raw.githubusercontent.com/xps-genesis/openpilot/Xps_Community_Release/panda/board/safety_declarations.h;
+cd /data/openpilot/_backup/mdpsH && wget https://raw.githubusercontent.com/gugupilot/openpilot/rel2/panda/board/safety_declarations.h;
 cd /data/openpilot/_backup/mdpsH && cp safety_declarations.h /data/openpilot/panda/board;
 
-cd /data/openpilot/_backup/mdpsH && wget https://raw.githubusercontent.com/xps-genesis/openpilot/Xps_Community_Release/panda/board/safety/safety_defaults.h;
+cd /data/openpilot/_backup/mdpsH && wget https://raw.githubusercontent.com/gugupilot/openpilot/rel2/panda/board/safety/safety_defaults.h;
 cd /data/openpilot/_backup/mdpsH && cp safety_defaults.h /data/openpilot/panda/board/safety;
 
-cd /data/openpilot/_backup/mdpsH && wget https://raw.githubusercontent.com/xps-genesis/openpilot/Xps_Community_Release/panda/board/safety/safety_hyundai_community.h;
+cd /data/openpilot/_backup/mdpsH && https://raw.githubusercontent.com/gugupilot/openpilot/rel2/panda/board/safety/safety_hyundai_community.h;
 cd /data/openpilot/_backup/mdpsH && cp safety_hyundai_community.h /data/openpilot/panda/board/safety;
 
 cd /data/openpilot/panda; pkill -f boardd; PYTHONPATH=..; python -c "from panda import Panda; Panda().flash()"; reboot
