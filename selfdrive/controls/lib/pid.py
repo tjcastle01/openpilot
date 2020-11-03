4b64b7293a9cef3f027d1602fc003b8fe493ca74
gugupilot <73624237+gugupilot@users.noreply.github.com>
2020-11-02 20:58:48 -0500

    Update pid.py

--- selfdrive/controls/lib/pid.py
+++ selfdrive/controls/lib/pid.py
@@ -5,7 +5,7 @@ GainSaS_BP = [0., 1.9, 2., 5., 10., 20., 40.]
 Gain_g = [0.15, .024, .025, .085, .12, .14, .16]
 
 GainV_BP = [0., 20., 20.01, 30.]
-Gain_V = [0.3, .5, .65, 1.2]
+Gain_V = [0.3, .5, .65, 1.5]
 
 def apply_deadzone(error, deadzone):
   if error > deadzone:
